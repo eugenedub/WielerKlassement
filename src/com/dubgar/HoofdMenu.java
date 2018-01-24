14ed4ee1171b8cc6fd20d1e90a1950dcacc8f330
ev20005 <eugene@genwiki.nl>
2018-01-24 10:37:51 +0100

    Initial commit

---
+++ src/com/dubgar/HoofdMenu.java
@@ -0,0 +1,46 @@
+package com.dubgar;
+
+import java.util.Scanner;
+
+public class HoofdMenu {
+
+    void createMenu() {
+        System.out.println("Enter one of the following commands:");
+        System.out.println("1 - Inlezen Bestanden");
+        System.out.println("2 - Rapportage");
+        System.out.println("3 - Afsluiten");
+        Scanner scanChoice = new Scanner(System.in);
+        System.out.println();
+
+        int choiceentry = 0;
+        try {
+            do {
+                System.out.println("Enter \"1\", \"2\" or \"3\"");
+
+                choiceentry = scanChoice.nextInt();
+
+                switch (choiceentry) {
+                    case 1:
+                        System.out.println("De keuze was 1");
+                        System.out.println("Bestanden worden ingelezen");
+
+                        break;
+                    case 2:
+                        System.out.println("De keuze was 2");
+                        System.out.println("Rapportage wordt gemaakt");
+                        break;
+                    case 3:
+                        // .. exit programr
+                        break;
+                    default:
+                        System.out.println("Choice must be a value between 1 and 3.");
+                }
+            } while (choiceentry != 3);
+        }
+        catch(Exception e) {
+            System.out.printf("Keuze foutief dient numeriek te zijn : %s%n",choiceentry);
+        }
+    }
+
+
+}
---
+++ src/com/dubgar/Main.java
@@ -0,0 +1,10 @@
+package com.dubgar;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HoofdMenu fietsKeuze = new HoofdMenu();
+        fietsKeuze.createMenu();
+    }
+}
